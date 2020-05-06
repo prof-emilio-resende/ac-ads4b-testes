fbf92a2acbb7e6b326751566b5798dd36a2b30a3
Pedro Henrique <pedro.progsantos@gmail.com>
2020-05-06 20:36:30 -0300

    test_calculo_base.py

--- test_calculo_base.py
+++ test_calculo_base.py
@@ -1,8 +1,6 @@
 #Teste utilizando o retorno de uma função chamada CalculaInss
 
-from DeduzirDependentes.py import deduzir_dependentes
-from calculadora_ir import calcula_inss
-import pytest
+from DeduzirDependentes import deduzir_dependentes
 
 @pytest.mark.parametrize('salario, qntd_dependentes, expected', [(3000, 2, 2290.82)])
 def test_calcula(salario, qntd_dependentes, expected):
